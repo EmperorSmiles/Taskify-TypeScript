ec9532c7fc1ff223b257a3b01bb2b6bc295691d4
EmperorSmiles <iamfavoursmiles@gmail.com>
2023-11-24 14:17:26 +0100

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 import './App.css'
 import InputField from './components/InputField'
+import { Todo } from './modal';
+import ToDoList from './components/ToDoList';
+import { DragDropContext } from "react-beautiful-dnd";
 
 const App:React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
+  
+  const handleAdd = (e:React.FormEvent) => {
+    e.preventDefault();
 
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo('');
+    }
+  };
+
+
+  console.log(todos)
   return (
-    <div className='App'>
-      <span className='heading'>Testify</span>
-      <InputField />
-   </div>
+    <DragDropContext onDragEnd={() => {}}>
+      <div className='App'>
+        <span className='heading'>Taskify</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <ToDoList todos={todos} setTodos={setTodos}
+        completedTodos={completedTodos} setCompletedTodos={setCompletedTodos} />
+    </div>
+    </DragDropContext>
   )
 }
 
